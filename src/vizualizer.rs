e3bf98a0c497fdd3446b795e40816d4962fbda62
I'm just zeroday <aoyamalinux@gmail.com>
2024-05-18 23:52:55 +0300

    update

--- src/vizualizer.rs
+++ src/vizualizer.rs
@@ -43,40 +43,59 @@ impl App {
         &mut self,
         terminal: &mut Terminal<CrosstermBackend<io::Stdout>>,
     ) -> Result<(), Box<dyn Error>> {
-        let _chunks = Layout::default()
+        let chunks = Layout::default()
             .direction(Direction::Horizontal)
             .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
             .split(terminal.size()?);
 
-        let log_items: Vec<ListItem> = self
-            .logs
-            .entries
-            .iter()
-            .map(|entry| {
-                let level = match entry.level {
-                    LogFormatter::Info => Style::default().fg(Color::Blue),
-                    LogFormatter::Warning => Style::default().fg(Color::Yellow),
-                    LogFormatter::Error => Style::default().fg(Color::Red),
-                    LogFormatter::Trace => Style::default().fg(Color::White),
-                };
+        let mut info_items: Vec<ListItem> = Vec::new();
+        let mut warning_items: Vec<ListItem> = Vec::new();
+        let mut error_items: Vec<ListItem> = Vec::new();
+        let mut trace_items: Vec<ListItem> = Vec::new();
 
-                let message = Spans::from(vec![Span::styled(entry.message.clone(), level)]);
-                let metadata = Spans::from(vec![
-                    Span::styled(format!("{:<9}", entry.level.to_string()), level),
-                    Span::raw(" "),
-                    Span::styled(
-                        entry.date.clone(),
-                        Style::default().add_modifier(Modifier::ITALIC),
-                    ),
-                ]);
+        for entry in &self.logs.entries {
+            let level = match entry.level {
+                LogFormatter::Info => Style::default().fg(Color::Blue),
+                LogFormatter::Warning => Style::default().fg(Color::Yellow),
+                LogFormatter::Error => Style::default().fg(Color::Red),
+                LogFormatter::Trace => Style::default().fg(Color::White),
+            };
 
-                ListItem::new(vec![metadata, Spans::from(""), message])
-            })
-            .collect();
+            let message = Spans::from(vec![Span::styled(entry.message.clone(), level)]);
+            let metadata = Spans::from(vec![
+                Span::styled(format!("{}", entry.level), level),
+                Span::raw(" "),
+                Span::styled(
+                    entry.date.clone(),
+                    Style::default().add_modifier(Modifier::ITALIC),
+                ),
+            ]);
 
-        let logs_list = List::new(log_items)
-            .block(Block::default().borders(Borders::ALL).title("Logs"))
+            let item = ListItem::new(vec![metadata, Spans::from(""), message]);
+
+            match entry.level {
+                LogFormatter::Info => info_items.push(item),
+                LogFormatter::Warning => warning_items.push(item),
+                LogFormatter::Error => error_items.push(item),
+                LogFormatter::Trace => trace_items.push(item),
+            }
+        }
+
+        let info_list = List::new(info_items)
+            .block(Block::default().borders(Borders::ALL).title("Info Logs"))
             .start_corner(Corner::TopLeft);
+
+        let warning_list = List::new(warning_items)
+            .block(Block::default().borders(Borders::ALL).title("Warning Logs"))
+            .start_corner(Corner::TopRight);
+
+        let error_list = List::new(error_items)
+            .block(Block::default().borders(Borders::ALL).title("Error Logs"))
+            .start_corner(Corner::BottomLeft);
+
+        let trace_list = List::new(trace_items)
+            .block(Block::default().borders(Borders::ALL).title("Trace Logs"))
+            .start_corner(Corner::BottomRight);
 
         let stats_list = List::new(vec![
             ListItem::new(vec![Spans::from(format!(
@@ -100,18 +119,32 @@ impl App {
                 self.stats.trace_messages
             ))]),
         ])
-        .block(Block::default().borders(Borders::ALL).title("Stats"))
-        .start_corner(Corner::TopRight);
+        .block(Block::default().borders(Borders::ALL).title("Stats"));
 
         terminal.draw(|f| {
-            let size = f.size();
-            let chunks = Layout::default()
+            let rects = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(
+                    [
+                        Constraint::Percentage(25),
+                        Constraint::Percentage(25),
+                        Constraint::Percentage(25),
+                        Constraint::Percentage(25),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+
+            let stats_rect = Layout::default()
                 .direction(Direction::Vertical)
                 .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
-                .split(size);
+                .split(rects[0])[1];
 
-            f.render_widget(logs_list, chunks[0]);
-            f.render_widget(stats_list, chunks[1]);
+            f.render_widget(info_list, rects[0]);
+            f.render_widget(warning_list, rects[1]);
+            f.render_widget(error_list, rects[2]);
+            f.render_widget(trace_list, rects[3]);
+            f.render_widget(stats_list, stats_rect);
         })?;
 
         Ok(())
@@ -128,7 +161,7 @@ pub fn run_app(logs: Logs, stats: LogStats) -> eyre::Result<()> {
     let mut app = App::new(logs, stats);
 
     loop {
-        app.render(&mut terminal);
+        let _ = app.render(&mut terminal);
 
         if let Event::Key(key) = event::read()? {
             match key.code {
